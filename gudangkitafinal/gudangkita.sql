2609a5ca85ea082ab4880d09c18d6d8647433e9f
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2024-12-18 06:38:58 +0700

    Add files via upload

--- gudangkitafinal/gudangkita.sql
+++ gudangkitafinal/gudangkita.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2023 at 01:04 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.4.14
+-- Generation Time: Dec 18, 2024 at 12:37 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,7 +34,7 @@ CREATE TABLE `barang` (
   `Harga` double(15,2) DEFAULT NULL,
   `Satuan` varchar(15) NOT NULL,
   `TglAuditTerakhir` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -50,7 +50,7 @@ CREATE TABLE `barangdigudang` (
   `Keterangan` text DEFAULT NULL,
   `KodeGudang` int(11) NOT NULL,
   `KodeBarang` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -61,7 +61,7 @@ CREATE TABLE `barangdigudang` (
 CREATE TABLE `gudang` (
   `KodeGudang` int(11) NOT NULL,
   `Alamat` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -74,8 +74,16 @@ CREATE TABLE `pengguna` (
   `Password` varchar(30) NOT NULL,
   `NamaPengguna` varchar(50) NOT NULL,
   `Alamat` text DEFAULT NULL,
-  `waktudaftar` datetime DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `waktudaftar` datetime DEFAULT current_timestamp(),
+  `level` enum('Admin','Operator','Umum') NOT NULL DEFAULT 'Umum'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`KodeLogin`, `Password`, `NamaPengguna`, `Alamat`, `waktudaftar`, `level`) VALUES
+('Admin', 'Admin', 'Admin', NULL, '2024-12-18 06:37:01', 'Admin');
 
 --
 -- Indexes for dumped tables
